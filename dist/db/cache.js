9ea7d0af8ba46fe8e36d122f05777718b5b96714
burakaltinbicak <mslmbrkaltnbcak@gmail.com>
2026-05-05 16:23:03 +0300

    chore: dist folder removed and gitignore added

--- dist/db/cache.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sessionCache = exports.websiteCache = void 0;
-exports.websiteCache = new Set();
-exports.sessionCache = new Set();
